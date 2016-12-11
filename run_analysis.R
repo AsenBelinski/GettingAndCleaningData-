71a75d3e6add5682fb7014a63615f3d81f05b109
AsenBelinski <asen.belinski@gmail.com>
2016-12-11 23:37:02 +0200

    Second commit

--- run_analysis.R
+++ run_analysis.R
@@ -32,6 +32,24 @@ train.activity <- read.table(file="Samsung_data/train/y_test.txt")
 names(train.activity) <- "activity"
 train <- data.frame(train.subject, train.activity, train.measurements)
 
+#
+#
+#
 
 #   1. Merges the training and the test sets to create one data set.
-join.data <- rbind(test,train)
+merged_data <- rbind(test,train)
+
+
+#   2. Extracts only the measurements on the mean and standard deviation 
+# for each measurement.
+columns <- grep("mean|std|subject.ID|activity",names(merged_data))
+selected.data <- merged_data[,columns]
+
+
+#   3. Uses descriptive activity names to name the activities in the data set
+for(i in 1:length(activities$V1)) selected.data$activity <- gsub(i,activities$V2[i], selected.data$activity)
+
+
+#   4. Appropriately labels the data set with descriptive variable names.
+
+#   5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
